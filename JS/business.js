cd90ec5a9d5e9656ef2f29790fcf72e8bc6aad24
Eduardo Lago <eduardolagonunes@gmail.com>
2023-02-13 13:10:22 -0300

    Finished

--- JS/business.js
+++ JS/business.js
@@ -5,29 +5,37 @@ function vantagenspar(){
     if(maisVantagens.style.display === "inline") {
         maisVantagens.style.display = "none";
         btnVantagens.innerHTML = "Saiba mais";
+        btnVantagens.style.backgroundColor = "black";
     }else{
         maisVantagens.style.display = "inline";
         btnVantagens.innerHTML = "Minimizar";
+        btnVantagens.style.backgroundColor = "#e1b55e";
     }
 }
 
 function vantagenspar1(){   
     let maisVantagens = document.getElementById("vantagens-parceiro1");
+    let textoVantagens = document.getElementById("texto-vant-par1");
 
     if(maisVantagens.style.display === "inline") {
-        maisVantagens.style.display = "none";        
+        maisVantagens.style.display = "none";
+        textoVantagens.style.color = "#f13239";
     }else{
-        maisVantagens.style.display = "inline";        
+        maisVantagens.style.display = "inline";
+        textoVantagens.style.color = "#e1b55e";        
     }
 }
 
 function vantagenspar2(){   
     let maisVantagens = document.getElementById("vantagens-parceiro2");
-
+    let textoVantagens = document.getElementById("texto-vant-par2");
+    
     if(maisVantagens.style.display === "inline") {
-        maisVantagens.style.display = "none";        
+        maisVantagens.style.display = "none";
+        textoVantagens.style.color = "#f13239";
     }else{
-        maisVantagens.style.display = "inline";        
+        maisVantagens.style.display = "inline";
+        textoVantagens.style.color = "#e1b55e";        
     }
 }
 //____________________________________________________________________________________
@@ -39,45 +47,50 @@ function vantagensapo(){
     if(maisVantagens.style.display === "inline") {
         maisVantagens.style.display = "none";
         btnVantagens.innerHTML = "Saiba mais";
+        btnVantagens.style.backgroundColor = "black";
     }else{
         maisVantagens.style.display = "inline";
         btnVantagens.innerHTML = "Minimizar";
+        btnVantagens.style.backgroundColor = "#e1b55e";
     }
 }
 
 function vantagensapo1(){   
     let maisVantagens = document.getElementById("vantagens-apoiador1");
+    let textoVantagens = document.getElementById("vantagens-apoiador10");
 
     if(maisVantagens.style.display === "inline") {
         maisVantagens.style.display = "none";
-        btnVantagens.innerHTML = "Veja algumas vantagens";
+        textoVantagens.style.color = "#f13239";
     }else{
         maisVantagens.style.display = "inline";
-        btnVantagens.innerHTML = "Minimizar";
+        textoVantagens.style.color = "#e1b55e";        
     }
 }
 
 function vantagensapo2(){   
     let maisVantagens = document.getElementById("vantagens-apoiador2");
+    let textoVantagens = document.getElementById("vantagens-apoiador20");
 
     if(maisVantagens.style.display === "inline") {
         maisVantagens.style.display = "none";
-        btnVantagens.innerHTML = "Veja algumas vantagens";
+        textoVantagens.style.color = "#f13239";
     }else{
         maisVantagens.style.display = "inline";
-        btnVantagens.innerHTML = "Minimizar";
+        textoVantagens.style.color = "#e1b55e";        
     }
 }
 
 function vantagensapo3(){   
     let maisVantagens = document.getElementById("vantagens-apoiador3");
+    let textoVantagens = document.getElementById("vantagens-apoiador30");
 
     if(maisVantagens.style.display === "inline") {
         maisVantagens.style.display = "none";
-        btnVantagens.innerHTML = "Veja algumas vantagens";
+        textoVantagens.style.color = "#f13239";
     }else{
         maisVantagens.style.display = "inline";
-        btnVantagens.innerHTML = "Minimizar";
+        textoVantagens.style.color = "#e1b55e";        
     }
 }
 
@@ -90,93 +103,102 @@ function vantagenspat(){
     if(maisVantagens.style.display === "inline") {
         maisVantagens.style.display = "none";
         btnVantagens.innerHTML = "Saiba mais";
+        btnVantagens.style.backgroundColor = "black";
     }else{
         maisVantagens.style.display = "inline";
         btnVantagens.innerHTML = "Minimizar";
+        btnVantagens.style.backgroundColor = "#e1b55e";
     }
 }
 
 function vantagenspat1(){   
     let maisVantagens = document.getElementById("vantagens-patrocinador1");
+    let textoVantagens = document.getElementById("vantagens-patrocinador10");
 
     if(maisVantagens.style.display === "inline") {
         maisVantagens.style.display = "none";
-        btnVantagens.innerHTML = "Veja algumas vantagens";
+        textoVantagens.style.color = "#f13239";
     }else{
         maisVantagens.style.display = "inline";
-        btnVantagens.innerHTML = "Minimizar";
+        textoVantagens.style.color = "#e1b55e";        
     }
 }
 
 function vantagenspat2(){   
     let maisVantagens = document.getElementById("vantagens-patrocinador2");
+    let textoVantagens = document.getElementById("vantagens-patrocinador20");
 
     if(maisVantagens.style.display === "inline") {
         maisVantagens.style.display = "none";
-        btnVantagens.innerHTML = "Veja algumas vantagens";
+        textoVantagens.style.color = "#f13239";
     }else{
         maisVantagens.style.display = "inline";
-        btnVantagens.innerHTML = "Minimizar";
+        textoVantagens.style.color = "#e1b55e";        
     }
 }
 
 function vantagenspat3(){   
     let maisVantagens = document.getElementById("vantagens-patrocinador3");
+    let textoVantagens = document.getElementById("vantagens-patrocinador30");
 
     if(maisVantagens.style.display === "inline") {
         maisVantagens.style.display = "none";
-        btnVantagens.innerHTML = "Veja algumas vantagens";
+        textoVantagens.style.color = "#f13239";
     }else{
         maisVantagens.style.display = "inline";
-        btnVantagens.innerHTML = "Minimizar";
+        textoVantagens.style.color = "#e1b55e";        
     }
 }
 
 function vantagenspat4(){   
     let maisVantagens = document.getElementById("vantagens-patrocinador4");
+    let textoVantagens = document.getElementById("vantagens-patrocinador40");
 
     if(maisVantagens.style.display === "inline") {
         maisVantagens.style.display = "none";
-        btnVantagens.innerHTML = "Veja algumas vantagens";
+        textoVantagens.style.color = "#f13239";
     }else{
         maisVantagens.style.display = "inline";
-        btnVantagens.innerHTML = "Minimizar";
+        textoVantagens.style.color = "#e1b55e";        
     }
 }
 
 function vantagenspat5(){   
     let maisVantagens = document.getElementById("vantagens-patrocinador5");
+    let textoVantagens = document.getElementById("vantagens-patrocinador50");
 
     if(maisVantagens.style.display === "inline") {
         maisVantagens.style.display = "none";
-        btnVantagens.innerHTML = "Veja algumas vantagens";
+        textoVantagens.style.color = "#f13239";
     }else{
         maisVantagens.style.display = "inline";
-        btnVantagens.innerHTML = "Minimizar";
+        textoVantagens.style.color = "#e1b55e";        
     }
 }
 
 function vantagenspat6(){   
     let maisVantagens = document.getElementById("vantagens-patrocinador6");
+    let textoVantagens = document.getElementById("vantagens-patrocinador60");
 
     if(maisVantagens.style.display === "inline") {
         maisVantagens.style.display = "none";
-        btnVantagens.innerHTML = "Veja algumas vantagens";
+        textoVantagens.style.color = "#f13239";
     }else{
         maisVantagens.style.display = "inline";
-        btnVantagens.innerHTML = "Minimizar";
+        textoVantagens.style.color = "#e1b55e";        
     }
 }
 
 function vantagenspat7(){   
     let maisVantagens = document.getElementById("vantagens-patrocinador7");
+    let textoVantagens = document.getElementById("vantagens-patrocinador70");
 
     if(maisVantagens.style.display === "inline") {
         maisVantagens.style.display = "none";
-        btnVantagens.innerHTML = "Veja algumas vantagens";
+        textoVantagens.style.color = "#f13239";
     }else{
         maisVantagens.style.display = "inline";
-        btnVantagens.innerHTML = "Minimizar";
+        textoVantagens.style.color = "#e1b55e";        
     }
 }
 
@@ -189,9 +211,11 @@ function vantagenscol(){
     if(maisVantagens.style.display === "inline") {
         maisVantagens.style.display = "none";
         btnVantagens.innerHTML = "Saiba mais";
+        btnVantagens.style.backgroundColor = "black";
     }else{
         maisVantagens.style.display = "inline";
         btnVantagens.innerHTML = "Minimizar";
+        btnVantagens.style.backgroundColor = "#e1b55e";
     }
 }
 
